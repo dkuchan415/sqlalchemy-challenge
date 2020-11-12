85c6e374b989c3723fea9d161b5bb4f074f673f0
dkuchan415 <danielkuchan@yahoo.com>
2020-11-11 22:24:49 -0600

    Completed flask app

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+import datetime as dt
+
 from flask import Flask, jsonify
 
 #################################################
@@ -19,7 +21,7 @@ Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 Measurement = Base.classes.measurement
-Station = Base.classes.Station
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
@@ -32,14 +34,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-        """List all available api routes."""
+    """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end<br/>")
+        f"/api/v1.0/start/end<br/>"
+    )
+
+
+#Precipitation Path
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
@@ -51,12 +57,47 @@ def prcp():
     last_year = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date>date).\
         order_by(Measurement.date.asc()).all()
- 
+
+    for date, prcp in last_year:
+        last_year_dict = {}
+        last_year_dict ={date: prcp}
+        return jsonify(last_year_dict)
+    
+
 #Change list of tuples (date, perceip) to list of dictionaries where the key = date. Value = precip
 # Do in jupyter notebook first
 
+#Stations Path
 
+@app.route("/api/v1.0/stations")
+def stat():
+    session = Session(engine)
+#TOBS path
+    station_group = session.query(Measurement.station, func.count(Measurement.station)).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+
+    return jsonify(station_group)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+
+    date = dt.datetime(2016, 8, 23)
+
+    top_station_year = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date>date).\
+        filter(Measurement.station == 'USC00519281').\
+        order_by(Measurement.tobs.asc()).all()
+    
+    return jsonify(top_station_year)
+
+@app.route("/api/v1.0/start")
 def calc_temps(start_date, end_date):
+
+    session = Session(engine)
+
     """TMIN, TAVG, and TMAX for a list of dates.
     
     Args:
